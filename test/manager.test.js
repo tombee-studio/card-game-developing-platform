98ddfd164bd3294b214d6a3ff989bfae47f5644a
windows tomoya <tomomail1997@gmail.com>
2020-05-25 17:35:16 +0900

    playersとeventEmitterのテストを作成しました

--- test/manager.test.js
+++ test/manager.test.js
@@ -2,19 +2,23 @@ import {assert} from 'chai'
 import Manager from '../src/manager.js'
 
 describe("Manager", () => {
-    describe("#setPlayers", () => {
-        it("Managerは実態があります", () => {
+    describe('#Manager', () => {
+        it('Managerは実態があります', () => {
             assert.notDeepEqual(undefined, Manager)
         })
-        it("EventEmitterの実態を取り出すことができます", () => {
+    })
+    describe('#eventEmitter', () => {
+        it('EventEmitterの実態を取り出すことができます', () => {
             assert.notDeepEqual(undefined, Manager.eventEmtter)
         })
+    })
+    describe("#players", () => {
+        const PLAYERS = 3
         it('playersを設定せずにgetしたときErrorを出力します', () => {
             assert.throws(() => { const a = Manager.players }, 'プレイヤー人数を設定してください')
         })
-        it("should return '3'", () => {
+        it(`should return ${PLAYERS}`, () => {
             Manager.eventEmtter.on('プレイヤー人数を設定', (data) => {
-                const PLAYERS = 3
                 assert.equal(PLAYERS, Manager.players)
                 assert.equal(`プレイヤー人数を${PLAYERS}人に設定しました`, data)
             })
